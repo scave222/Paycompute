f8a6aceea76d4cce9c97cc4980313a1be6a8aa22
Caleb Okogbe <okogbecaleb@gmail.com>
2021-01-07 20:53:56 +0100

    Layout, Index-EmployeeController

--- Paycompute/Startup.cs
+++ Paycompute/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Paycompute.Persistence;
+using Paycompute.Services;
+using Paycompute.Services.Implementation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,6 +37,7 @@ namespace Paycompute
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddScoped<IEmployeeService, EmployeeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
